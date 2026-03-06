b841b4c744ec911a786d18d9f52404146558a467
Conor McManamon <c.s.mcmanamon@gmail.com>
2026-03-06 14:49:02 -0500

    Updating close settings indicator

--- src/StopPage.tsx
+++ src/StopPage.tsx
@@ -329,7 +329,7 @@ export default function StopPage() {
                 role="status"
                 aria-live="polite"
               >
-                Click to close
+                Tap to close
               </span>
             )}
             <button
